e0783b1d4e35361aec298cf0afb54e2d06c2c010
Sean Fong <seancfong@gmail.com>
2018-12-08 21:26:59 -0800

    Add navbar

---
+++ index.js
@@ -0,0 +1,26 @@
+var navbar = document.getElementById("navbar");
+var navbar_topics = document.getElementById("navbar-topics");
+var navbar_topics_links = navbar_topics.getElementsByTagName("A");
+var sticky = navbar.offsetTop;
+
+window.onscroll = function() {
+  // console.log(window.pageYOffset);
+  // console.log(sticky);
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  if (navbar.classList.contains("sticky")) {
+    navbar.style.backgroundColor = "#555";
+    for (var i = 0; i < navbar_topics_links.length; i++){
+      navbar_topics_links[i].style.color = "#fff";
+    }
+  } else {
+    navbar.style.backgroundColor = "#ccc";
+    for (var i = 0; i < navbar_topics_links.length; i++){
+      navbar_topics_links[i].style.color = "#000";
+    }
+  }
+};
